7e05f4a9a0fbe24e62a41623004ae0d1c5a51e96
OlhaIrhizova <irhizovaolha@gmail.com>
2024-11-22 13:25:16 +0000

    Deploying to gh-pages from @ OlhaIrhizova/goit-js-hw-09@4d4d28881bedd26134f8169ee7b210756b2984d4 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/styles-DVnwr6kK.js";const o={email:"",message:""},a=document.querySelector(".feedback-form"),r="feedback-form-state";localStorage.getItem(JSON.stringify(o));a.addEventListener("input",n);function n(e){e.preventDefault();const t=e.target.value;localStorage.setItem(r,t)}
+//# sourceMappingURL=2-form.js.map
